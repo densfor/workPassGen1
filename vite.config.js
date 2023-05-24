bee4adff8e36c5df832804315d5dff49a1148e50
densfor <yz5645@gmail.com>
2023-05-24 13:48:45 +0300

    sec2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/workPassGen/',
+  base: '/workPassGen1/',
   plugins: [vue()],
   resolve: {
     alias: {
